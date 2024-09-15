4d6ff2b76b7a7e8c86d5b2cc28208defc0e153e1
Aamir041 <aamirsaudagar34@gmail.com>
2024-09-15 10:49:33 +0530

    removed typeid from header while publishing

--- publisher/src/main/java/com/publisher/demo/service/PublisherService.java
+++ publisher/src/main/java/com/publisher/demo/service/PublisherService.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.rabb    it.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
